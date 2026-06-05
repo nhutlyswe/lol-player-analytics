292f25a3d58af85922a0b882ec131270208b479c
nhutlyswe <nhutlyswe@gmail.com>
2026-06-04 23:42:39 -0500

    feat: added styling for nav bar (#14)

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+    const pathname = usePathname();
+
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.inner}>
+                <div className={styles.left}>
+                    <Link 
+                        href="/" 
+                        className={`${styles.link} ${pathname === "/" ? styles.active : ""}`}
+                    >
+                        Scout
+                    </Link>
+                </div>
+
+                <div className={styles.right}>
+                    <Link 
+                        href="/draft" 
+                        className={`${styles.link} ${pathname === "/draft" ? styles.active : ""}`}
+                    >
+                        Draft Assistant
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    );
+}
